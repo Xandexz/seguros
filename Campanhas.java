24e33b588a8cb381cc36b08cc32cb700d2dfe6df
Alexandre <alex.martins.eu@gmail.com>
2023-12-06 22:01:21 +0000

    Administrador ciclos for e Id de campanhas

--- Campanhas.java
+++ Campanhas.java
@@ -1,4 +1,25 @@
 
 public class Campanhas {
+    private String idCampanhas;
+
     
+    
+    public Campanhas(String idCampanhas) {
+        this.idCampanhas = idCampanhas;
+    }
+
+
+    public String getIdCampanhas() {
+        return idCampanhas;
+    }
+
+    public void setIdCampanhas(String idCampanhas) {
+        this.idCampanhas = idCampanhas;
+    }
+
+    
+    @Override
+    public String toString() {
+        return "Campanhas [idCampanhas=" + idCampanhas + "]";
+    }
 }
